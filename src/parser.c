6ccddebb0bd559ff7b7a3144f880927916c9d9bc
Helder Mauricio Da Silva Cabo <heda-sil@c1r16s6.42porto.com>
2023-05-01 11:33:24 +0100

    input validation done

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: heda-sil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 13:16:45 by heda-sil          #+#    #+#             */
-/*   Updated: 2023/04/28 14:35:04 by heda-sil         ###   ########.fr       */
+/*   Updated: 2023/05/01 11:32:05 by heda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,20 @@ int	validator(char **input)
 	while (input[++i])
 	{
 		j = i;
-		while (input[++j])
+		while (input[++j]) //check duplicates
 			if (!ft_strncmp(input[i], input[j], ft_strlen(input[i])))
 				return (1);
+		j = -1;
+		while (input[i][++j])
+		{
+			if (input[i][j] == '-' && !j)
+			{
+				if (!ft_isdigit(input[i][j + 1]))
+					return (1);
+			}
+			else if (!ft_isdigit(input[i][j]))
+				return (1);
+		}
 	}
-		
 	return (0);
 }

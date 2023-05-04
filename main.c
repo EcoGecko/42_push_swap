140a9556f44e1d2f11ae23e053009789c60dd9b6
Helder Mauricio da Silva Cabo <heda-sil@student.42porto.com>
2023-05-05 00:00:39 +0100

    Updated gitignore and some main.c formatting

--- main.c
+++ main.c
@@ -3,36 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heda-sil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: heda-sil <heda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:53:28 by heda-sil          #+#    #+#             */
-/*   Updated: 2023/05/04 15:02:51 by heda-sil         ###   ########.fr       */
+/*   Updated: 2023/05/04 23:51:53 by heda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	print_stack(t_stack *stack) //REMOVE tmp function to print stacks
+void	print_stack(t_stack *stack) // REMOVE tmp function to print stacks
 {
-	t_list		*tmp;
+	t_list	*tmp;
 
 	if (stack)
 	{
 		tmp = stack->stack;
 		while (tmp)
 		{
-			ft_printf("[%d]-[%d]\n", ((t_info *)tmp->content)->value, ((t_info *)tmp->content)->index);
+			ft_printf("[%d]-[%d]\n", ((t_info *)tmp->content)->value,
+				((t_info *)tmp->content)->index);
 			tmp = tmp->next;
 		}
 	}
-}	
+}
 
-int	main(int argc, char *argv[])//run prog as ARGS="" ./a.out ${=ARGS}
+int	main(int argc, char *argv[]) // run prog as ARGS="" ./a.out ${=ARGS}
 {
 	t_data	data;
 
 	if (argc < 2)
+	{
 		return (EXIT_FAILURE);
+	}
 	if (validator(argv))
 	{
 		ft_putendl_fd("ERROR", STDERR_FILENO);
@@ -42,9 +45,9 @@ int	main(int argc, char *argv[])//run prog as ARGS="" ./a.out ${=ARGS}
 	data.b = create_stack(argc - 1, 0);
 	populate_stack(data.a, argv + 1);
 	get_final_index(data.a);
-//	print_stack(data.a);
+	//	print_stack(data.a);
 	sorter(&data);
-//	print_stack(data.a);
+	//	print_stack(data.a);
 	clean_data(&data);
 	return (EXIT_SUCCESS);
 }

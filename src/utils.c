878a7b5d0f56fbef544c802826f88763d558d1b1
Helder Mauricio Da Silva Cabo <heda-sil@c1r15s1.42porto.com>
2023-05-13 12:06:36 +0100

    BUG - when input was much larger than int max/min it would overflow long long limit and not exit on error

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: heda-sil <heda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 11:38:53 by heda-sil          #+#    #+#             */
-/*   Updated: 2023/05/12 20:21:29 by heda-sil         ###   ########.fr       */
+/*   Updated: 2023/05/13 12:03:46 by heda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	clean_stack(t_stack *stack)
 }
 
 /*Validates input, checks for duplicates, and for non integer arguments*/
-int	validator(char **input) //?DO i need to verify when input is ""
+int	validator(char **input)
 {
 	int	i;
 	int	j;
@@ -51,15 +51,27 @@ int	validator(char **input) //?DO i need to verify when input is ""
 			if (!ft_strncmp(input[i], input[j], ft_strlen(input[i])))
 				return (1);
 		}
-		j = -1;
-		while (input[i][++j])
-		{
-			if (input[i][j] == '-' && j == 0)
-				j++;
-			if (!ft_isdigit(input[i][j]) || \
-			(ft_atol(input[i]) > INT_MAX || ft_atol(input[i]) < INT_MIN))
-				return (1);
-		}
+		if (check_values(input[i]))
+			return (1);
+	}
+	return (0);
+}
+
+int	check_values(char *input)
+{
+	int	i;
+
+	if (ft_strlen(input) > 11)
+		return (1);
+	if (ft_atol(input) > INT_MAX || ft_atol(input) < INT_MIN)
+		return (1);
+	i = -1;
+	while (input[++i])
+	{
+		if (input[i] == '-' && i == 0)
+			i++;
+		if (!ft_isdigit(input[i]))
+			return (1);
 	}
 	return (0);
 }
